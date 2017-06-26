b6df303cedb5cff5f56c50ef900ee0a1c2cc5ac5
anoff <offenhaeuser@gmail.com>
2017-06-26 16:55:51 +0200

    :star2: feat: function in folder

---
+++ collector/index.js
@@ -0,0 +1,20 @@
+const request = require('request-promise')
+
+module.exports = function (context, timer) {
+  const url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,SC,LTC,ETC,XMR,XRP&tsyms=BTC,EUR,USD'
+  context.bindings.tableBinding = []
+  request(url)
+    .then(data => {
+      data = JSON.parse(data)
+      const entry = {
+        PartitionKey: 'test',
+        RowKey: Date.now()
+      }
+      Object.keys(data).forEach(key => {
+        entry[key] = data[key]
+      })
+      context.log(entry)
+      context.bindings.tableBinding.push(entry)
+      context.done(null, data)
+    })
+}
---
+++ collector/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Set up the azure space for collector function
+# Prereqs: 
+#   1. have `azurecli` python package installed (az binary)
+
+RESOURCE_GROUP="cryptobooks1"
+STORAGE_NAME="oracle"
+FUNCTION_APP="collector"
+
+az group create --name "$RESOURCE_GROUP" --location westeurope
+
+az storage account create --name "$STORAGE_NAME" --location westeurope --resource-group "$RESOURCE_GROUP" --sku Standard_LRS
+
+az functionapp create --name "$FUNCTION_APP" --storage-account "$STORAGE_NAME"  --resource-group "$RESOURCE_GROUP" --consumption-plan-location westeurope
+
+az functionapp deployment source config --name "$FUNCTION_APP" --resource-group "$RESOURCE_GROUP" --repo-url https://github.com/anoff/cryptobooks --branch master --manual-integration
