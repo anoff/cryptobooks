96ce95a60128f6ac34ba641bfd256d79c7acb133
anoff <offenhaeuser@gmail.com>
2017-06-26 23:12:34 +0200

    :star2: feat: new table layout & table name

--- collector/index.js
+++ collector/index.js
@@ -1,19 +1,24 @@
 const request = require('request-promise')
 
 module.exports = function (context, timer) {
+  const timestamp = new Date()
   const url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,SC,LTC,ETC,XMR,XRP&tsyms=BTC,EUR,USD'
   context.bindings.tableBinding = []
   request(url)
     .then(data => {
       data = JSON.parse(data)
-      const entry = {
-        PartitionKey: 'test',
-        RowKey: Date.now()
-      }
-      Object.keys(data).forEach(key => {
-        entry[key] = data[key]
+      Object.keys(data).forEach(coin => {
+        const entry = {
+          PartitionKey: coin,
+          RowKey: timestamp.valueOf(),
+          timeRecorded: timestamp.toISOString()
+        }
+        Object.keys(data[coin]).forEach(curr => {
+          entry[curr] = data[coin][curr]
+        })
+        context.bindings.tableBinding.push(entry)
       })
-      context.bindings.tableBinding.push(entry)
       context.done(null, data)
     })
+    .catch(e => context.done(e))
 }
