efea304d0c0bf7df8acc783881cb911c10320507
anoff <offenhaeuser@gmail.com>
2017-07-01 22:10:59 +0200

    :star2: feat: PUT method and introduce `azure-storage` dependency

--- users/index.js
+++ users/index.js
@@ -1,13 +1,10 @@
-function checkAuth (req, dbEntry) {
-  if (!req.headers || !dbEntry) {
-    return false
-  }
-  const accessKey = req.headers['access-key']
-  return dbEntry.accessKey === accessKey
-}
+const TABLENAME = 'users'
 
+const azure = require('azure-storage')
+
+// check if accessKey is correct, otherwise respond with 401 immediately
 function isAuth (req, dbEntry, context) {
-  if (!checkAuth(req, dbEntry)) {
+  if (!req.headers || !dbEntry || dbEntry.accessKey !== req.headers['access-key']) {
     context.res = {
       status: 401,
       body: { msg: 'Invalid accessKey' }
@@ -20,6 +17,7 @@ function isAuth (req, dbEntry, context) {
 }
 
 module.exports = function (context, req) {
+  // shorthand for sending a response
   function send (body) {
     if (typeof body === 'string') {
       context.res = {
@@ -33,14 +31,15 @@ module.exports = function (context, req) {
 
   const method = req.method.toUpperCase()
   const dbEntry = context.bindings.usersIn
-  const dbOut = context.bindings.usersOut = []
   const username = req.params.name
   const accessKey = req.headers['access-key']
+  const tableService = azure.createTableService(process.env.AzureWebJobsStorage)
 
-  context.log(dbEntry)
   switch (method) {
     case 'GET':
       if (isAuth(req, dbEntry, context)) {
+        delete dbEntry.RowKey
+        delete dbEntry.PartitionKey
         send({body: dbEntry})
       }
       break
@@ -49,31 +48,55 @@ module.exports = function (context, req) {
       if (!dbEntry) {
         // new users needs to come with accessKey
         if (!accessKey) {
-          send({status: 401, body: { msg: 'Please specify an accessKey' }})
+          send({status: 401, body: {msg: 'Please specify an accessKey'}})
         } else {
           // TODO check body size/type to prevent abuse
-          dbOut.push({
+          const entry = {
             PartitionKey: 'users',
             RowKey: username,
             timeCreated: new Date(),
             timeUpdated: new Date(),
             accessKey,
             assets: req.body
+          }
+          tableService.insertEntity(TABLENAME, entry, function (error, result, response) {
+            if (!error) {
+              send('User created')
+            } else {
+              send({status: 400, body: {msg: error}})
+            }
           })
-          send('User created')
         }
       // user already exists
       } else {
         send({
           status: 400,
-          body: { msg: 'Username already taken' }
+          body: {msg: 'Username already taken'}
         })
+      }
+      break
+    case 'PUT':
+      if (!dbEntry) {
+        send({status: 404, body: {msg: 'Username does not exist. Use POST to create new'}})
+      } else {
+        if (isAuth(req, dbEntry, context)) {
+            // TODO check body size/type
+          dbEntry.assets = req.body
+          dbEntry.timeUpdated = new Date()
+          tableService.replaceEntity(TABLENAME, dbEntry, function (error, result, response) {
+            if (!error) {
+              send('Assets updated')
+            } else {
+              send({status: 400, body: {msg: error}})
+            }
+          })
+        }
       }
       break
     default:
       send({
         status: 500,
-        body: { msg: 'Unknown operation' }
+        body: {msg: 'Unknown operation'}
       })
   }
 }
