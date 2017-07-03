7a99ebe9f0a56252ae0beed0e3a37d9389de97cd
anoff <offenhaeuser@gmail.com>
2017-07-03 17:11:36 +0200

    :scissors: refactor: heat the 'main' function

--- users-heater/index.js
+++ users-heater/index.js
@@ -2,7 +2,7 @@ const https = require('https')
 
 module.exports = function (context, myTimer) {
   var start = new Date()
-  return https.get('https://cbtraindev.azurewebsites.net/api/bert', res => {
+  return https.get('https://cryptobooks.azurewebsites.net/api/johndoe', res => {
     res.on('end', () => {
       context.log(`Done after ${new Date() - start} ms`)
       context.done()
