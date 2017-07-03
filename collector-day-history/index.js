043d2ce207e0833e46e5badfe830cf3ec0a8f356
anoff <offenhaeuser@gmail.com>
2017-07-03 23:54:42 +0200

    :star2: feat: switch on collector
    #4

--- collector-day-history/index.js
+++ collector-day-history/index.js
@@ -1,13 +1,10 @@
 const request = require('request-promise')
-const azure = require('azure-storage')
-const TABLENAME = 'prices'
 
 const fiats = ['EUR', 'BTC', 'USD']
-const coins = ['BTC']//, 'ETH', 'SC', 'LTC', 'ETC', 'XMR', 'XRP']
+const coins = ['ETH']//, 'BTC', 'SC', 'LTC', 'ETC', 'XMR', 'XRP']
 
 module.exports = function (context, timer) {
-  const timestamp = new Date()
-  //const tableService = azure.createTableService(process.env.AzureWebJobsStorage)
+  context.bindings.tableBinding = []
   coins.forEach(coin => {
     Promise.all(fiats.map(fiat => {
       const url = `https://min-api.cryptocompare.com/data/histoday?fsym=${coin}&tsym=${fiat}&limit=2000`
@@ -26,9 +23,26 @@ module.exports = function (context, timer) {
       })
     }))
     .then(arr => {
-      const results = arr.reduce((p, c) => p.concat(c), [])
-      context.log(results)
-      // TODO reduce to one line and add to tableoutpu
+      const master = arr.splice(0, 1)[0]
+      const seconds = arr.reduce((p, c) => p.concat(c), [])
+      const entries = master.map(e => {
+        const obj = {
+          PartitionKey: e.coin,
+          RowKey: e.date.toISOString(),
+          timeRecorded: e.date,
+          historic: true
+        }
+        obj[e.fiat] = e.price
+        // append other exchange rates here
+        seconds
+        .filter(o => o.date.valueOf() === e.date.valueOf())
+        .forEach(o => {
+          obj[o.fiat] = o.price
+        })
+        return obj
+      })
+      .filter(e => e[fiats[0]] > 0) // filter elements where currency does not report valid price
+      context.bindings.tableBinding = context.bindings.tableBinding.concat(entries)
     })
   })
 }
