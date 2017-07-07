b1de86b8b40baed810659c9c5a894debf76487b0
anoff <offenhaeuser@gmail.com>
2017-07-08 00:07:38 +0200

    :star2: feat: hourly/daily columns during update
    #10

--- collector-min/index.js
+++ collector-min/index.js
@@ -1,12 +1,21 @@
 const request = require('request-promise')
 const azure = require('azure-storage')
 const TABLENAME = 'prices'
+const INTERVAL = 5 * 60 // interval the function is running [s]
 
 module.exports = function (context, timer) {
   const timestamp = new Date()
   const url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,SC,LTC,ETC,XMR,XRP&tsyms=BTC,EUR,USD'
   const tableService = azure.createTableService(process.env.AzureWebJobsStorage)
 
+  function addEntry(entry) {
+    return tableService.insertEntity(TABLENAME, entry, function (error, result, response) {
+      if (error) {
+        context.log(error)
+        context.done(error)
+      }
+    })
+  }
   request(url)
     .then(data => {
       data = JSON.parse(data)
@@ -18,12 +27,15 @@ module.exports = function (context, timer) {
         Object.keys(data[coin]).forEach(curr => {
           entry[curr] = data[coin][curr]
         })
-        tableService.insertEntity(TABLENAME, entry, function (error, result, response) {
-          if (error) {
-            context.log(error)
-            context.done(error)
+        // add additional entry if hour is passed
+        if (timestamp.getMinutes() * 60 + timestamp.getSeconds() <= INTERVAL) {
+          entry.hourly = true
+          // add another entry every day
+          if (timestamp.getHours() === 0) {
+            entry.daily = true
           }
-        })
+        }
+        addEntry(entry)
       })
       context.done(null, data)
     })
